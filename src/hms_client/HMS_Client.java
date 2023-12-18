6410b6b74edbd77962fc23a4d4bdbc866ca6f74e
Mazen212237 <mazen212237@bue.edu.eg>
2023-12-18 19:00:11 +0200

    Added Controller and WIP on RMI

---
+++ src/hms_client/HMS_Client.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hms_client;
+
+import controllers.LoginController;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author gamer
+ */
+public class HMS_Client {
+
+    public static void main(String[] args) throws RemoteException {
+        LoginWindow gui = new LoginWindow();
+        gui.setLocationRelativeTo(null);
+        gui.setVisible(true);
+
+        Registry r= LocateRegistry.getRegistry(1099);
+
+        LoginController gui_Controller= new LoginController(gui,r);
+    }
+    
+}
